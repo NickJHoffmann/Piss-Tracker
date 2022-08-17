25ab3bc2127998da6152d3379833c9b6fd593083
Nick Hoffmann <59148770+NickJHoffmann@users.noreply.github.com>
2022-08-16 22:31:29 -0400

    Refactor User model import before db was defined

--- app.py
+++ app.py
@@ -8,14 +8,9 @@ from flask_login import LoginManager
 from dotenv import load_dotenv
 import os
 
-from models import User
-
-from auth import auth as auth_blueprint
-from main import main as main_blueprint
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
-
 
 # def create_app():
 app = Flask(__name__)
@@ -30,15 +25,20 @@ login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 login_manager.init_app(app)
 
+from models import User
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
 
+from auth import auth as auth_blueprint
 # blueprint for auth routes in our app
 app.register_blueprint(auth_blueprint)
 
+from main import main as main_blueprint
 # blueprint for non-auth parts of app
 app.register_blueprint(main_blueprint)
+
+
 if __name__ == "__main__":
     app.run()
